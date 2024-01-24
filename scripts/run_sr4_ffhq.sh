f7e0f22e9012c785511c5eb7fbd486b22ca1ab9f
Xiangming Meng <mengxm11@gmail.com>
2024-01-24 21:19:00 +0800

    Update run_sr4_ffhq.sh

--- scripts/run_sr4_ffhq.sh
+++ scripts/run_sr4_ffhq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for((i=0; i<1; i++)); do {
-	python3 main.py --gpu 0  --task_config  'configs/sr4_config.yaml' --model_config 'configs/model_config.yaml'  --save_dir './saved_results'
+	python3 main.py --gpu 1  --task_config  'configs/sr4_config.yaml' --model_config 'configs/model_config.yaml'  --save_dir './saved_results'
   echo "DONE!"
 } & done
 wait
